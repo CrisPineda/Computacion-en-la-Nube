5ff2003deaa8798fc84788cf586c3c233804587e
Cristian Camilo Pineda <62514345+CrisPineda@users.noreply.github.com>
2025-08-29 15:00:26 -0500

    Miniproyecto 1

---
+++ MiniProyecto_1/scripts/provision-haproxy-consul.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Actualizar sistema
+echo "Actualizando sistema..."
+apt-get update
+apt-get upgrade -y
+
+# Instalar HaProxy
+
+apt-get update
+apt-get install -y haproxy
+
+
+# Instalar Consul
+echo "Instalando Consul server..."
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update -y
+sudo apt install consul -y
+
+
+
+#-----------------------------------
+
